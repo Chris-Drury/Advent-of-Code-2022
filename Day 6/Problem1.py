61f8b9d606299c638cff1e3a3513ac990cb2b14e
Chris Drury <chrswrdrury@gmail.com>
2022-12-06 08:56:31 -0500

    Add Day 6

---
+++ Day 6/Problem1.py
@@ -0,0 +1,28 @@
+import os
+
+# Open file
+__location__: str = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+input_name: str = __location__ + "\\" +  "input.txt"
+
+# Read each line into an element
+input: str = open(input_name, "r").readline()
+
+marker: str
+
+# Initialize to cover initial 4 chars
+packet_index: int = 4
+while packet_index < len(input):
+    marker = input[(packet_index - 4):packet_index]
+
+    unique_marker = True
+    for char in marker:
+        if marker.count(char) > 1:
+            unique_marker = False
+            break
+
+
+
+    if unique_marker:
+        print("unique marker: ", marker, " at index: ", packet_index)
+        break
+    packet_index += 1
